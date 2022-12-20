4b7a9454571d76338d97b1ea8998bdcbe48d0c2d
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-12-20 10:40:48 +0100

    - add hover color change - separate app button to standalone widget - make homepage sufficient

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,45 +1,171 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:portfolio/util/constants/app_colors.dart';
+import 'package:portfolio/util/constants/app_images.dart';
+import 'package:portfolio/util/constants/app_sizes.dart';
+import 'package:portfolio/util/models/theme_color_provider.dart';
 import 'package:portfolio/util/constants/app_text_style.dart';
-import 'package:portfolio/widgets/header/header.dart';
+import 'package:portfolio/widgets/app_buttons.dart';
 
-// import 'package:portfolio/widgets/header/header.dart' deferred as head;
-
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth < AppSizes.maxMobile) {
+        // mobile header
+        return const _HeaderDesign(
+          aspectRatio: 1,
+          padding: 20,
+        );
+      } else if (constraints.maxWidth < AppSizes.maxTablet) {
+        // tablet header
+        return const _HeaderDesign(
+          aspectRatio: 2,
+          padding: 100,
+        );
+      } else {
+        // desktop header
+        return const _HeaderDesign(
+          aspectRatio: 2.2,
+          padding: 200,
+        );
+      }
+    });
+  }
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HeaderDesign extends StatelessWidget {
+  final double aspectRatio;
+  final double padding;
+  const _HeaderDesign({
+    Key? key,
+    required this.aspectRatio,
+    required this.padding,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+        body: SizedBox(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Stack(
         children: [
-          const HeaderWidget(),
-          // Align(
-          //   alignment: Alignment(0, -0.5),
-          //   child: SizedBox(
-          //     child: DefaultTextStyle(
-          //       style: AppTextStyle.primary(context, color: Colors.white),
-          //       child: AnimatedTextKit(
-          //         repeatForever: true,
-          //         animatedTexts: [
-          //           TypewriterAnimatedText('Hello I am Anthony!',
-          //               speed: const Duration(
-          //                 milliseconds: 100,
-          //               )),
-          //         ],
-          //         onTap: () {
-          //           print("Tap Event");
-          //         },
-          //       ),
-          //     ),
-          //   ),
-          // ),
+          SizedBox(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            child: Stack(
+              children: [
+                ColorFiltered(
+                  colorFilter: ColorFilter.mode(
+                      ThemeColorProvider.of(context).color.withOpacity(0.7),
+                      BlendMode.color),
+                  child: Image.network(
+                    AppImages.codeBg,
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: padding),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hello I am Anthony!',
+                        style:
+                            AppTextStyle.primary(context, color: Colors.white),
+                      ),
+                      SizedBox(
+                        height: padding,
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: <Widget>[
+                            Text(
+                              'I Build',
+                              style: AppTextStyle.title(context,
+                                  color: Colors.white),
+                            ),
+                            SizedBox(width: padding / 4),
+                            DefaultTextStyle(
+                              style: AppTextStyle.title(context,
+                                  color: Colors.white),
+                              child: AnimatedTextKit(
+                                repeatForever: true,
+                                animatedTexts: [
+                                  RotateAnimatedText('ANDROID'),
+                                  RotateAnimatedText('IOS'),
+                                  RotateAnimatedText('FLUTTER'),
+                                ],
+                                onTap: () {
+                                  print("Tap Event");
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: padding / 2),
+                      Row(
+                        children: [
+                          AppButtons.primary(
+                              title: 'About me', onPressed: () {}),
+                          SizedBox(width: padding),
+                          AppButtons.primary(
+                              title: 'My Works', onPressed: () {}),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment(0.98, 0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    // children:
+                    //     AppColors.primaryColors.mapIndexed(((index, element) {
+                    //   return colorCircles(context, index);
+                    // })).toList(),
+                    children: List.generate(AppColors.primaryColors.length,
+                        (index) => colorCircles(context, index)),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
+      ),
+    ));
+  }
+
+  Widget colorCircles(BuildContext context, int index) {
+    double size = MediaQuery.of(context).size.width;
+    if (size < 500) {
+      size = size / 10;
+    } else {
+      size = size / 20;
+    }
+    return InkWell(
+      onTap: () {
+        ThemeColorProvider.of(context, listen: false).setColorIndex(index);
+      },
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          border: ThemeColorProvider.of(context).selectedIndex == index
+              ? Border.all(
+                  width: 7,
+                  color: Colors.white,
+                  strokeAlign: StrokeAlign.outside)
+              : null,
+          shape: BoxShape.circle,
+          color: AppColors.primaryColors[index],
+        ),
       ),
     );
   }
