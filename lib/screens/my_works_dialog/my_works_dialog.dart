08ce44fd3890bc09b217d3c4e9fa44bac69d2190
Anthony Aniobi <anthonyaniobi198@gmail.com>
2023-01-01 21:28:52 +0100

    scaffold for my works dialog

---
+++ lib/screens/my_works_dialog/my_works_dialog.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyWorksDialog extends StatelessWidget {
+  const MyWorksDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
